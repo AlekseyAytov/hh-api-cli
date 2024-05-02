2fa6e114a75745013096a24af25b56e97ad779a9
AlekseyAytov <aytov88@gmail.com>
2024-05-02 13:30:02 +0300

    update: deleted previous Search and Analizer classes

--- analize_results.py
+++ analize_results.py
@@ -5,183 +5,13 @@ from read_config import Settings
 from typing import NamedTuple
 from errors import OpenResumeError
 from save_results import Storage
+from models import Item, Contact
 
 class ItemsCounter(NamedTuple):
     amount  : int
     opened  : int
     closed  : int
     can_open: int
-
-class ItemInfo(NamedTuple):
-    title   : str
-    age     : str
-    area    : str
-    open_url: str
-
-
-class Analizer:
-
-    __parsed_data: list[dict] = []
-    __raw_data:    list[dict] = []
-
-    def __init__(self, settings: Settings, storage: Storage, comment: str = None, open_limit: int = None) -> None:
-        self.settings = settings
-        self.storage = storage
-        self.comment = comment
-        self.open_limit = open_limit
-    
-    def analize(self, from_data):
-        self.__analize(from_data)
-        
-    def __make_request(self, resume_url):
-        response = requests.request("GET", resume_url, headers=self.settings.headers)
-
-        if response.status_code == 200:
-            page_json   = response.json()
-            return (page_json, None)
-        else:
-            print(f'Ошибка: {response.status_code} - {response.reason}. {response.json()}')
-            return (None, response.status_code)
-        
-    def __open_resume(self, url):
-        page_data, error_code = self.__make_request(url)
-        match error_code:
-            case None:
-                print("Резюме открыто.")
-                self.__raw_data.append(page_data)
-                self.__parse_resume(page_data)
-            case 403:
-                print("Требуется авторизация пользователя")
-                # raise OpenResumeError
-            case 404:
-                print("Резюме не существует или недоступно для текущего пользователя")
-            case 429:
-                print("Для работодателя превышен лимит просмотров резюме в сутки")
-                # raise OpenResumeError
-            case _:
-                raise OpenResumeError
-    
-    def __analize(self, items) -> list[dict]:
-        counter: ItemsCounter = self.__make_items_counter(items)
-        print(f"Результаты поиска: загружено {counter.amount} из них {counter.opened} открыто, {counter.closed} закрыто для просмотра контактов.")
-        print(f"Возможны к открытию: {counter.can_open} резюме.")
-
-        interruption_flag = False
-        continue_flag = False
-        open_counter = 0
-        for count, item in enumerate(items):
-            if interruption_flag:
-                print("Прерывание...")
-                break
-
-            if  self.open_limit and open_counter >=  self.open_limit:
-                print(f"Достигнут лимит открытий резюме - {self.open_limit}.")
-                break
-            
-            info: ItemInfo = self.__make_item_info(item)
-            if info.open_url:
-                while True:
-                    if not continue_flag:
-                        question = input(f'Открыть контакты резюме-{count+1}: {info.title}, возраст {info.age}, регион {info.area}? (y-yes/n-no/s-stop/all-open all): ')
-                    else:
-                        print(f"{count+1}")
-                        question = "y"
-
-                    match question:
-                        case "y":
-                            print("Открываем...")
-                            try:
-                                open_counter += 1
-                                self.__open_resume(info.open_url)
-                            except Exception as err:
-                                self.storage.save_to_excel(self.__parsed_data)
-                                raise err
-                            break
-                        case "n":
-                            print("Пропускаем...")
-                            break
-                        case "s":
-                            interruption_flag = True
-                            break
-                        case "all":
-                            continue_flag = True
-                        case _:
-                            print("Некорректный ввод...")
-        
-        self.storage.save_to_excel(self.__parsed_data)
-        # self.__save_raw_data()
-    
-    def __make_items_counter(self, items) -> ItemsCounter:
-        opened = 0
-        closed = 0
-        can_open = 0
-        for item in items:
-            if item.get("can_view_full_info", False):
-                opened += 1
-            else:
-                closed += 1
-            
-            if item.get("actions", False) and item["actions"].get("get_with_contact", False):
-                can_open += 1
-
-        return ItemsCounter(amount=len(items), opened=opened, closed=closed, can_open=can_open)
-
-    def __make_item_info(self, item) -> ItemInfo:
-        title = item.get("title", "")
-        age = item.get("age", "")
-        area = item["area"]["name"] if item.get("area", False) else ""
-        open_url = item["actions"]["get_with_contact"]["url"] if item.get("actions", False) and item["actions"].get("get_with_contact", False) else None
-        return ItemInfo(title=title, age=age, area=area, open_url=open_url)
-
-    # распарсить номер мобльного
-    def __parse_phone_number(self, contacts) -> str:
-        for i in contacts:
-            if i["type"]["id"] == "cell":
-                country_code = i["value"]["country"]
-                city_code = i["value"]["city"]
-                number = i["value"]["number"]
-                return country_code + city_code + number
-        
-    # распарсить каждое резюме
-    def __parse_resume(self, resume):
-        resume_id = resume["id"]
-
-        last_name   = resume["last_name"]   if resume["last_name"]   else "н/д"
-        first_name  = resume["first_name"]  if resume["first_name"]  else "н/д"
-        middle_name = resume["middle_name"] if resume["middle_name"] else "н/д"
-        name = last_name + ' ' + first_name + ' ' + middle_name
-
-        area = resume["area"]["name"] if resume["area"] else "н/д"
-        age = resume["age"] if resume["age"] else "н/д"
-        wish_title = resume["title"] if resume["title"] else "н/д"
-
-        cell_phone = self.__parse_phone_number(resume["contact"])
-        timestr = time.strftime('%d.%m.%Y')
-
-        link = resume["alternate_url"]
-
-        self.__parsed_data.append({
-            "Date"   : timestr,
-            "ID"     : resume_id,
-            "Phone"  : cell_phone,
-            "Name"   : name,
-            "Area"   : area,
-            "Age"    : age,
-            "Title"  : wish_title,
-            "url"    : link,
-            "comment": self.comment,
-        })
-
-    def __save_raw_data(self):
-        if len(self.__raw_data) > 0:
-            with open("raw_data_of_last_analize.json", "w", encoding='utf8') as fh:
-                json.dump(self.__raw_data, fh, ensure_ascii=False)
-
-
-# NOTE: Вариант класса Analizer с типами pydantic  
-
-from models import Item, Contact
-
 class AnalizerTyped:
 
     __parsed_data: list[dict] = []
@@ -331,22 +161,7 @@ class AnalizerTyped:
                 json.dump(self.__raw_data, fh, ensure_ascii=False)
 
 
-
 if __name__ == '__main__':
-    # with open("debug_data/test_data_2.json", "r", encoding='utf8') as fh:
-    #     data = json.load(fh)
-
-    # s = Settings()
-    # storage = Storage(settings=s)
-    # comment = ", ".join(["one", "1", "two"])
-    # analizer = Analizer(settings=s, storage=storage, comment=comment, open_limit=2)
-    # analizer = analizer.analize(data)
-
-    # if len(data) > 0:
-    #     with open("test_data_resume_2.json", "w", encoding='utf8') as fh:
-    #         json.dump(data, fh, ensure_ascii=False)
-
-
     # Для тестирования класса AnalizerTyped забрать json из файла
     with open("debug_data/test_data_2.json", "r", encoding='utf8') as fh:
         data = json.load(fh)
--- get_search_results.py
+++ get_search_results.py
@@ -3,72 +3,6 @@ from read_config import Settings
 from read_filter import SearchParams
 from errors import SearchRequestError
 import json
-
-class Search:
-    PER_PAGE = 100
-    MAX_PAGE = int(2000/PER_PAGE) - 1
-
-    all_data: list = []
-
-    def __init__(self, settings: Settings, filter: dict) -> None:
-        self.settings = settings
-        self.payload = filter
-        self.__set_per_page()
-
-    def get_search_results(self):
-        self.__do_search()
-        return self.all_data
-    
-    def __make_request(self):
-        api_url = "resumes"
-        url = self.settings.base_url + api_url
-        # print(self.payload)
-
-        response = requests.request("GET", url, headers=self.settings.headers, data=self.payload)
-
-        if response.status_code == 200:
-            page_json   = response.json()
-            return (page_json, None)
-        else:
-            print(f'Ошибка: {response.status_code} - {response.reason}. {response.json()}')
-            return (None, response.status_code)
-
-    def __do_search(self) -> None:
-        while True:
-            page_data, error_code = self.__make_request()
-
-            match error_code:
-                case None:
-                    pages_quantity = page_data["pages"]
-                    page_number = page_data["page"]
-                    items_found = page_data["found"]
-                    # print(f"pages_quantity{pages_quantity} page_number{page_number} items_found{items_found}")
-                    print(f"По данным параметрам поиска найдено всего резюме: {items_found}. Подгружено страниц: {page_number+1}")
-
-                    self.all_data.extend(page_data["items"])
-
-                    match page_number:
-                        case self.MAX_PAGE:
-                            print("Достигнут лимит одновременного просмотра в 2000 резюме.")
-                            break
-                        case x if x < pages_quantity-1:
-                            self.payload["page"] = page_number+1
-                        case _:
-                            break
-                case 400:
-                    print("Ошибка в фильтре запроса.")
-                    break
-                case 403:
-                    print("Доступ не разрешен.")
-                    break
-                case _:
-                    raise SearchRequestError
-                
-    def __set_per_page(self) -> None:
-        self.payload["per_page"] = self.PER_PAGE
-
-
-# NOTE: Вариант класса Search с типами pydantic  
 from models import PageResults, Item
 
 class SearchTyped:
@@ -146,7 +80,6 @@ if __name__ == '__main__':
     
     s = Settings()
     f = SearchParams(s).get_filter()
-    # searcher = Search(settings=s, filter=f)
     searcher = SearchTyped(settings=s, filter=f)
     data = searcher.get_search_results()
 
